0a6413fcb00ec6744fd4b2d10573f4af5940ec5c
Pavel Drabek <pazzicz@gmail.com>
2018-07-26 17:02:14 +0200

    first commit

---
+++ Assets/Scripts/Cuttable.cs
@@ -0,0 +1,52 @@
+﻿using BLINDED_AM_ME;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK.Examples;
+
+public class Cuttable : MonoBehaviour {
+
+    public Material cutMaterial;
+    public Collider collider;
+    public float timeCreated;
+
+    private void Awake()
+    {
+        collider = GetComponent<Collider>();
+        timeCreated = Time.time;
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(Time.time < timeCreated + 0.5f) {
+            return;
+        }
+
+        var sword = collision.attachedRigidbody.GetComponent<CuttingSword>();
+        if(sword == null) {
+            return;
+        }
+
+        var mat = cutMaterial == null ? sword.cutMaterial : cutMaterial;
+
+        var objs = MeshCut.Cut(gameObject, sword.transform.position, sword.transform.right, mat);
+
+        PreparePart(objs[0]).AddForce(-transform.right * sword.splitStrenght);
+        PreparePart(objs[1]).AddForce(transform.right * sword.splitStrenght);
+
+        if(collider != null) {
+            Destroy(gameObject);
+        }
+    }
+
+    private Rigidbody PreparePart(GameObject o)
+    {
+        o.layer = gameObject.layer;
+        var mc = o.AddComponent<MeshCollider>();
+        mc.sharedMesh = o.GetComponent<MeshFilter>().sharedMesh;
+        mc.convex = true;
+        var cuttable = o.AddComponent<Cuttable>();
+        cuttable.cutMaterial = cutMaterial;
+        return o.AddComponent<Rigidbody>();
+    }
+}
---
+++ Assets/Scripts/CuttingSword.cs
@@ -0,0 +1,73 @@
+﻿namespace VRTK.Examples
+{
+    using BLINDED_AM_ME;
+    using UnityEngine;
+
+    public class CuttingSword : VRTK_InteractableObject
+    {
+        private float impactMagnifier = 120f;
+        private float collisionForce = 0f;
+        private float maxCollisionForce = 4000f;
+        private VRTK_ControllerReference controllerReference;
+
+        public Material cutMaterial;
+        public float splitStrenght;
+
+        public float CollisionForce()
+        {
+            return collisionForce;
+        }
+
+        public override void Grabbed(VRTK_InteractGrab grabbingObject)
+        {
+            base.Grabbed(grabbingObject);
+            controllerReference = VRTK_ControllerReference.GetControllerReference(grabbingObject.controllerEvents.gameObject);
+        }
+
+        public override void Ungrabbed(VRTK_InteractGrab previousGrabbingObject)
+        {
+            base.Ungrabbed(previousGrabbingObject);
+            controllerReference = null;
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            controllerReference = null;
+            interactableRigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        }
+
+        private void OnTriggerEnter(Collision collision)
+        {
+            if(VRTK_ControllerReference.IsValid(controllerReference) && IsGrabbed()) {
+                collisionForce = VRTK_DeviceFinder.GetControllerVelocity(controllerReference).magnitude * impactMagnifier;
+                var hapticStrength = collisionForce / maxCollisionForce;
+                VRTK_ControllerHaptics.TriggerHapticPulse(controllerReference, hapticStrength, 0.5f, 0.01f);
+            } else {
+                collisionForce = collision.relativeVelocity.magnitude * impactMagnifier;
+            }
+        }
+
+        protected override void FixedUpdate()
+        {
+            base.FixedUpdate();
+
+            if(!IsGrabbed()) {
+                return;
+            }
+
+            var controllerEvents = GetGrabbingObject().GetComponent<VRTK_ControllerEvents>();
+            if(controllerEvents != null && controllerEvents.IsButtonPressed(VRTK_ControllerEvents.ButtonAlias.TouchpadPress)) {
+                var axis = controllerEvents.GetAxis(VRTK_ControllerEvents.Vector2AxisAlias.Touchpad);
+
+                if(axis.y > 0.5) {
+                    splitStrenght += 0.05f;
+                }
+                if(axis.y < -0.5) {
+                    splitStrenght -= 0.05f;
+                }
+                Debug.Log("new splitStrenght: " + splitStrenght);
+            }
+        }
+    }
+}
